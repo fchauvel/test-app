6ff74b59ad61c2df0ca1211cbdea4bbebc3df02f
Franck Chauvel <franck.chauvel@sintef.no>
2020-11-03 13:17:56 +0000

    Fix: Move download into GeneSIS deployment file

--- install.sh
+++ install.sh
@@ -7,17 +7,9 @@ export FC_APP_VERSION="${APP_VERSION}"
 export FC_APP_LIFESPAN="${APP_LIFESPAN}"
 
 apt-get update
-apt-get install --yes git python3-pip python3-virtualenv
-
-
-# fetch the sources
-git clone https://github.com/fchauvel/test-app.git
-
-cd test-app
+apt-get install --yes python3-pip python3-virtualenv
 
 virtualenv .venv -p /usr/bin/python3
-
 source .venv/bin/activate
-
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 
