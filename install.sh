a4b762d48d04a007cbab990a3217745dd69840b4
Franck Chauvel <franck.chauvel@sintef.no>
2020-11-05 05:14:30 +0000

    Fix install of virtualenv

--- install.sh
+++ install.sh
@@ -7,7 +7,9 @@ export FC_APP_VERSION="${APP_VERSION}"
 export FC_APP_LIFESPAN="${APP_LIFESPAN}"
 
 apt-get update
-apt-get install --yes python3-pip python3-virtualenv
+apt-get -qq install --yes --no-install-recommends python3-pip
+
+pip3 install virtualenv
 
 virtualenv .venv -p /usr/bin/python3
 source .venv/bin/activate
