a855bbdf4a0d879d99fe90ae2c7ac12af2af2d0b
Franck Chauvel <franck.chauvel@sintef.no>
2021-02-04 14:37:30 +0000

    Add 'files.pythonhosted.org' to the trusted hosts

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ export FC_APP_LIFESPAN="${APP_LIFESPAN}"
 apt-get update
 apt-get -qq install --yes --no-install-recommends python3-pip
 
-pip3 install virtualenv
+pip3 --trusted-host pypi.python.org --trusted-host files.pythonhosted.org install virtualenv
 
 virtualenv .venv -p /usr/bin/python3
 source .venv/bin/activate
-pip install --trusted-host pypi.python.org -r requirements.txt
+pip3 install --trusted-host pypi.python.org --trusted-host files.pythonhosted.org -r requirements.txt
 
