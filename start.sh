78a4da9526c36f3881152cd48e3079c79cc0114d
Franck Chauvel <franck.chauvel@sintef.no>
2021-02-05 09:48:56 +0000

    Start the app in the foreground

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Start the application, listen for HTTP requests on port 5000 by
+# default
 source .venv/bin/activate
-nohup python app.py > log.txt 2>&1 &
-deactivate
+
+# /!\ Don't run this in the background or the container will exit as
+#     soon as the shell command returns
+python app.py
+
+
